e9616e8a2295cde706cd8eab9cca4887b0faa30d
boris <borispovod@tutanota.com>
2016-05-15 22:51:50 +0300

    Subscribe, new chart, bot face, etc

--- js/jquery.wings.js
+++ js/jquery.wings.js
@@ -33,10 +33,6 @@ $(document).ready(function(){
 	});
 
 
-	/* Chart.Js */
-
-	  	Chart.defaults.global.legend.display = false;
-
   		var randomScalingFactor = function() {
 			return Math.round(Math.random() * 100);
 		};
@@ -46,57 +42,102 @@ $(document).ready(function(){
 		var randomColor = function(opacity) {
 			return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
 		};
-		var config = {
-			type: 'pie',
-			scaleShowLabels: false,
-			data: {
-				datasets: [{
-					data: [
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor(),
-						randomScalingFactor()
-					],
-					backgroundColor: [
-						"#F7464A",
-						"#46BFBD",
-						"#FDB45C",
-						"#949FB1"
-					],
-				}],
-				labels: [
-					"Red",
-					"Green",
-					"Yellow",
-					"Dark Grey"
-				]
-			},
-			options: {
-			  	responsive: true,
-			  	animationSteps: 100,
-			  	animationEasing: 'easeInOutQuart'
-			}
+
+  		var pieConfig = {
+		  "size": {
+			 "canvasHeight": 400,
+			 "canvasWidth": 590,
+			 "pieOuterRadius": "78%"
+		  },
+		  "data": {
+			 "content": [
+				{
+				  "label": "Test 1",
+				  "value": 8,
+				  "color": "#7e3838"
+				},
+				{
+				  "label": "Test 2",
+				  "value": 5,
+				  "color": "#7e6538"
+				},
+				{
+				  "label": "Test 4",
+				  "value": 2,
+				  "color": "#7c7e38"
+				},
+				{
+				  "label": "Test 5",
+				  "value": 3,
+				  "color": "#587e38"
+				}
+			 ]
+		  },
+		  "labels": {
+			 "outer": {
+				"pieDistance": 25
+			 },
+			 "inner": {
+				"format": "none"
+			 },
+			 "mainLabel": {
+				"color": "#FFF",
+				"font": "verdana"
+			 },
+			 "percentage": {
+				"color": "#FFF",
+				"font": "verdana",
+				"decimalPlaces": 0
+			 },
+			 "value": {
+				"color": "#FFF",
+				"font": "verdana"
+			 },
+			 "lines": {
+				"enabled": true,
+				"color": "#FFF"
+			 },
+			 "truncation": {
+				"enabled": true
+			 }
+		  },
+		  "effects": {
+			 "pullOutSegmentOnClick": {
+				"effect": "linear",
+				"speed": 400,
+				"size": 8
+			 }
+		  }
 		};
 
-		window.onload = function() {
-			var ctx = document.getElementById("chart-area").getContext("2d");
-			window.myPie = new Chart(ctx, config);
-		};
+  		function renderPie() {
+		  window.pie = new d3pie("chart-area", pieConfig);
+		}
 
 		$('.chart-changer a').click(function() {
-			$(this).parent().children('.active').removeClass('active');
-			$(this).addClass('active');
-			$.each(config.data.datasets, function(i, piece) {
-				$.each(piece.data, function(j, value) {
-					config.data.datasets[i].data[j] = randomScalingFactor();
-					config.data.datasets[i].backgroundColor[j] = randomColor(0.7);
-				});
-			});
-			window.myPie.update();
+		  $(this).parent().children('.active').removeClass('active');
+		  $(this).addClass('active');
+
+		  var pieData = pieConfig.data.content;
+		  var newData = [];
+		  pieData.forEach(function (i) {
+			 newData.push({
+				label: i.label,
+				value: randomScalingFactor(),
+				color: randomColor(0.7)
+			 });
+		  });
+
+		  window.pie.updateProp("data.content", newData);
 		});
 
+		$("#interactiv").on('slide.bs.carousel', function(evt) {
+		  var nextSlide = $(evt.relatedTarget).index();
 
-	/* Connected carousels */
+		  if (nextSlide == 1 && !window.pie) {
+			 renderPie();
+		  }
+		});
 
 		$('#interactiv .carousel-control.left').on('click',function(e){
 			e.preventDefault();
@@ -134,43 +175,40 @@ $(document).ready(function(){
 				}
 			});
 
-
-	/* CHAT */
-
 	var chat = [
 		{
 			'author': 'bot',
-			'avatar': 'images/tmp-face.jpg',
+			'avatar': 'images/bot.png',
 			'msg': 'Welcome to Wings DAO Bot. We allow to fund and manage your DAOs easily.'
 		},
 		{
 			'author': 'user',
-			'avatar': 'images/tmp-face.jpg',
+			'avatar': 'images/face.png',
 			'msg': '/start'
 		},
 	  	{
 		  'author': 'bot',
-		  'avatar': 'images/tmp-face.jpg',
+		  'avatar': 'images/bot.png',
 		  'msg': 'List of commands: <br><br> /daolist - List of DAOs<br> /instruction - Instruction how to invest<br> /mybalance - Your DAO balance<br> /transfer - Transfer your BTC/ETH to dao<br> /terms - Terms & Conditions<br> /help - Help'
 		},
 	  	{
 		  'author': 'user',
-		  'avatar': 'images/tmp-face.jpg',
+		  'avatar': 'images/face.png',
 		  'msg': '/mybalance'
 		},
 	  	{
 		  'author': 'bot',
-		  'avatar': 'images/tmp-face.jpg',
+		  'avatar': 'images/bot.png',
 		  'msg':  "You've invested in 3 DAOs: <br><br> - Wings DAO<br> - Demo DAO<br> - SuperCool DAO<br><br>Write DAO name to get more info"
 		},
 		 {
 			'author': 'user',
-			'avatar': 'images/tmp-face.jpg',
+			'avatar': 'images/face.png',
 			'msg': 'Wings DAO'
 		 },
 	   {
 		  'author': 'bot',
-		  'avatar': 'images/tmp-face.jpg',
+		  'avatar': 'images/bot.png',
 		  'msg':  "You have 100k Wings DAO Tokens, you've earned 1000% profit"
 		}
 	];
@@ -192,7 +230,6 @@ $(document).ready(function(){
 
 	function chatting() {
 	  function postMessage(msg, cb) {
-		 console.log("post message");
 		 if (msg.author === "bot") {
 			setTimeout(function () {
 			  $('#interactive ul').append('<li class="' + msg.author + '"><img src="' + msg.avatar + '"><div class="msg">' + msg.msg + '</div></li>');
@@ -276,5 +313,33 @@ $(document).ready(function(){
 		  $("#" + i).text($.i18n.prop(i));
 		}
 	 }
+  });
+
+  $("#subscribe-form").submit(function (e) {
+	 e.preventDefault();
+	 $("#subscribe_error").hide();
+	 $("#subscribe_successful").hide();
+
+	 var email = $("#subscribe-email").val();
+
+	 $.ajax({
+		type: "POST",
+		url: "http://localhost:3030/subscribe",
+		data: JSON.stringify({
+		  email: email
+		}),
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function () {
+		  $("#subscribe_successful").show();
+		},
+		error: function (resp) {
+		  var defaultError = "Something went wrong, contact us: support@wings.ai";
+		  var error = resp.responseJSON? resp.responseJSON.error || defaultError: defaultError;
+
+		  $("#subscribe_error").text(error);
+		  $("#subscribe_error").show();
+		}
+	 })
   });
 });
